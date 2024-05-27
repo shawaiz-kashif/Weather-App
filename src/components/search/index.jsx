844ac5dd0ef528a8a1fa66f07f13fb12f7245d71
shawaiz-kashif <shawaiz608@gmail.com>
2024-05-27 22:52:47 +0500

    first commit

---
+++ src/components/search/index.jsx
@@ -0,0 +1,18 @@
+
+
+
+export default function Search({ search, setSearch, handleSearch }) {
+
+    return <div className="search-engine">
+        <input
+            type="text"
+            placeholder="Enter City Name"
+            name="search"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+        />
+        <button onClick={handleSearch}>
+            Search
+        </button>
+    </div>
+}
